34411b6cf1617536d01ee13f7977e03a0a1bfcb5
javi-lopez02 <mjlphone0210@gmail.com>
2025-06-08 01:58:26 -0400

    update

--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:8000",
+      "http://new.rankingmejoresplayas.com",
     ],
     credentials: true,
   })
